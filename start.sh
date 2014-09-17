ab303057911fc1c29a06b9279b58043e4237f2be
Martijn van Maurik <martijn@vmaurik.nl>
2014-09-17 08:59:09 +0200

    Add some logics

--- start.sh
+++ start.sh
@@ -10,11 +10,11 @@ then
    cp -rp /opt/b3/b3/.extplugins /data/extplugins
 fi
 
-if [[ -L "/opt/b3/b3/conf" ]]
+if [[ ! -L "/opt/b3/b3/conf" ]]
 then
    ln -s /data/conf /opt/b3/b3/conf
 fi
-if [[ -L "/opt/b3/b3/extplugins" ]]
+if [[ ! -L "/opt/b3/b3/extplugins" ]]
 then
    ln -s /data/extplugins /opt/b3/b3/extplugins
 fi
