d12455778d4855392251ca360c5ed897c443c9c3
Freekers <1370857+Freekers@users.noreply.github.com>
2019-07-12 22:41:44 +0200

    Added mapping for parsers

--- start.sh
+++ start.sh
@@ -9,6 +9,10 @@ if [[ ! -d "/data/extplugins" ]]
 then
    cp -rp /opt/b3/b3/.extplugins /data/extplugins
 fi
+if [[ ! -d "/data/parsers" ]]
+then
+   cp -rp /opt/b3/b3/.parsers /data/parsers
+fi
 
 if [[ ! -L "/opt/b3/b3/conf" ]]
 then
@@ -17,6 +21,10 @@ fi
 if [[ ! -L "/opt/b3/b3/extplugins" ]]
 then
    ln -s /data/extplugins /opt/b3/b3/extplugins
+fi
+if [[ ! -L "/opt/b3/b3/parsers" ]]
+then
+   ln -s /data/parsers /opt/b3/b3/parsers
 fi
 
 /usr/bin/python /opt/b3/b3_run.py $@
